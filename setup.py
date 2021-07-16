f8035e55824895e94530b1fe591c843b8e4a358e
yuanfengji <yuanfengji@tencent.com>
2021-07-16 10:51:43 +0800

    other docs

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='MCTrans',
+    version='0.0.1',
+    author='Yuanfeng Ji, Jiamin Ren',
+    author_email='u3008013@connect.hku.hk',
+    description='Simple Framework for Medcial Image Analysis',
+    url='https://github.com/JiYuanFeng/MCTrans',
+    packages=setuptools.find_packages(),
+    license='Internal',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: Internal',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ]
+)
