69525d2cb3aa79655f9c8bd6a65e75a93daa7ad7
yuanfengji <yuanfengji@tencent.com>
2021-07-21 17:40:23 +0800

    fix error config

--- tools/train.sh
+++ tools/train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CFG="configs/debug/unet_vgg32_d5_256x256_400ep_pannuke.py"
+CFG="configs/pannuke-vgg32/unet_vgg32_d5_256x256_400ep_pannuke.py"
 WORK_DIR=$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/
 python3 tools/train.py ${CFG} --work-dir ${WORK_DIR} --gpus 1
 
